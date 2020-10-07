abecb3dc83a6bb15d5994b33a8aee70a2cf29c3e
Carly Sills <carlysills@Carlys-MacBook-Pro-8.local>
2020-10-06 22:43:01 -0400

    real init commit

--- main.java
+++
@@ -1,9 +0,0 @@
-import project.*;
-
-public class main {
-    public static void main(String[] args){
-        TrainModel train = new TrainModel();
-        TrainController controller = new TrainController(train);
-        controller.printPassengerCount();
-    }
-}
