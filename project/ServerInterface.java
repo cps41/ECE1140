b2dd2021bcffaa7e1b9b5e2a56fac2cde1a61c6b
Carly Sills <carlysills@Carlys-MacBook-Pro-8.local>
2020-10-07 19:55:06 -0400

    here goes

---
+++ project/ServerInterface.java
@@ -0,0 +1,20 @@
+package project;
+
+/* Needed on both Server and Client */
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ServerInterface extends Remote {
+    int updateInterval = 100;
+
+    void serverSend(String key, Object value) throws RemoteException;
+
+    void serverSend(String key, Object value, boolean burnAfterReading) throws RemoteException;
+
+    Object serverReceive(String key) throws RemoteException;
+
+    void serverCall(String key, Object... argv) throws RemoteException;
+
+    Object[] serverGetCall(String key) throws RemoteException;
+}
