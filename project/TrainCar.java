f10a4e5b97bb6fee34f0e551ee6a3418e2c4efc8
Carly Sills <carlysills@Carlys-MacBook-Pro-8.local>
2020-10-06 22:43:01 -0400

    real init commit

---
+++ project/TrainCar.java
@@ -0,0 +1,15 @@
+package project;
+
+
+public class TrainCar {
+    private static int PASSENGER_COUNT;
+
+    public TrainCar() {
+        PASSENGER_COUNT = 8;
+    }
+	public int getPassengerCount() {
+		return PASSENGER_COUNT;
+	}
+    
+}
+
--- project/TrainModel.java
+++ project/TrainModel.java
@@ -1,9 +1,54 @@
 package project;
 
+import java.util.*;
 
 public class TrainModel {
-    public static int PASSENGER_COUNT;
+    private static float AUTHORITY; // in meters
+    private static float LENGTH;
+    private static LinkedList<TrainCar> CARS = new LinkedList<>();
+    private static float MASS; // in tons
+    private static int CREW_COUNT;
+    private static int PASSENGER_COUNT;
+    private final float CAR_LENGTH = 32.2f; // in meters
+    private final float CAR_HEIGHT = 3.42f; // in meters
+    private final float CAR_WIDTH = 2.65f; // in meters 
+
     public TrainModel() {
-        PASSENGER_COUNT = 128;
+        AUTHORITY = 30.5f;
+        for (int i=0; i<=5; i++) CARS.add(new TrainCar());
+        LENGTH = CAR_LENGTH*CARS.size();
+        MASS = 56.7f*CARS.size(); //in tons
+        CREW_COUNT = 12;
+        for (int j=0; j<CARS.size(); j++) {
+            PASSENGER_COUNT += CARS.get(j).getPassengerCount();
+        }
+    }
+
+    public float getAuthority() {
+        return AUTHORITY;
+    }
+
+    public float getLength() {
+        return LENGTH;
+    }
+
+    public float getWidth() {
+        return CAR_WIDTH;
+    }
+
+    public float getHeight() {
+        return CAR_HEIGHT;
+    }
+
+    public float getMass() {
+        return MASS;
+    }
+
+    public int getPassengerCount() {
+        return PASSENGER_COUNT;
+    }
+
+    public int getCrewCount() {
+        return CREW_COUNT;
     }
 }
