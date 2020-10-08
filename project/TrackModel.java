56a10502d8a322b37619d6b9c8ce4be835d25471
agh37 <70410938+agh37@users.noreply.github.com>
2020-10-08 13:53:39 -0400

    Update TrackModel.java

--- project/TrackModel.java
+++ project/TrackModel.java
@@ -2,12 +2,30 @@ package project;
 
 public class TrackModel 
 {
-	private static int[] blockLength;
-	private static int[] blockGrade;
-	private static int[] speedLimit;
-	private static int[] elevation;
-	private static boolean[] switchPosition;
-	//private static double[] beaconData;
+	//set for blue - must still extend for green and red line
+	
+	//infrustructure
+	static boolean[] stations;
+	static boolean[] switches;
+	static boolean[] crossings;
+	
+	//detail
+	static int[] blockLength;
+	static int[] speedLimit;
+	static int[] blockGrade;
+	static int[] cumulativeElevation;
+	
+	//status
+	static boolean[] switchPosition;
+	//static boolean[] crossingStatus;  
+	static boolean[] trackStatus; //broken rail condition
+	
+	//data
+	//static double[] beaconData;
+	
+	//passed data
+	static int speed;
+	static int authority;
 	
 	
 	public static void main(String[] args)
@@ -16,8 +34,16 @@ public class TrackModel
 		
 		//initialize for blue line
 		switchPosition = new boolean[1];
-		
 	}
 	
+	public void getSwitchPosition(String line, int block)//track only get switch status, not set switch
+	{
+		return true;
+	}
+	
+	public void getCrossingStatus(String line, int section)
+	{
+		return false;
+	}
 	
 }
