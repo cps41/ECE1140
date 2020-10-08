fcfd3c543d766c155b52dadd68f427d9053a4f11
agh37 <70410938+agh37@users.noreply.github.com>
2020-10-08 14:31:25 -0400

    1

--- project/Client.java
+++ project/Client.java
@@ -9,7 +9,12 @@ public class Client {
     private static final String cpuIP = "3.131.133.188";
     private static ServerInterface mini;
     private static boolean connected = false;
-    public static void main(String[] args) {
+    
+    public static void main(String[] args) 
+    {
+    	TrackModel model = new TrackModel();
+    	
+    	
         send("key1", "value 1");
         send("key2", "value 2");
         send("key3", 500.0);
@@ -20,58 +25,89 @@ public class Client {
         System.out.printf("Server %s = %s\n", "key3", receive("key3"));
         System.out.printf("Server %s = %s\n", "key4", Arrays.toString((int[]) receive("key4")));
     }
-    static boolean send(String key, Object value) {
-        if (!connected) {            connectClient();
+    
+    static boolean send(String key, Object value) 
+    {
+        if (!connected) 
+        {            
+        	connectClient();
         }
-        try {
+        try 
+        {
             mini.serverSend(key, value);
             return true;
-        } catch (Exception e) {
+        } 
+        catch (Exception e) 
+        {
             System.out.printf("Failed to set %s to %s\n", key, value);
             return false;
         }
     }
-    static Object receive(String key) {
-        if (!connected) {
+    
+    static Object receive(String key) 
+    {
+        if (!connected) 
+        {
             connectClient();
         }
-        try {
+        try 
+        {
             return mini.serverReceive(key);
-        } catch (Exception e) {
+        } 
+        catch (Exception e) 
+        {
             System.out.printf("Failed to receive %s\n", key);
         }
         return null;
     }
-    private static void connectClient() {
-        if (!connected) {
-            try {
+    
+    private static void connectClient() 
+    {
+        if (!connected) 
+        {
+            try
+            {
                 Registry registry = LocateRegistry.getRegistry(cpuIP, PORTNUMBER);
                 mini = (ServerInterface) registry.lookup("ServerInterface");
                 connected = true;
-            } catch (Exception e) {
+            } 
+            catch (Exception e) 
+            {
                 System.err.println("Client exception: " + e.toString());
                 e.printStackTrace();
             }
         }
     }
-    static void call(String key, Object...argv) {
-        if (!connected) {
+    
+    static void call(String key, Object...argv) 
+    {
+        if (!connected) 
+        {
             connectClient();
         }
-        try {
+        try 
+        {
             mini.serverCall(key, argv);
-        } catch (Exception e) {
+        } 
+        catch (Exception e) 
+        {
             System.err.println("Client exception: " + e.toString());
             e.printStackTrace();
         }
     }
-    static Object[] getCall(String key) {
-        if (!connected) {
+    
+    static Object[] getCall(String key) 
+    {
+        if (!connected) 
+        {
             connectClient();
         }
-        try {
+        try 
+        {
             return mini.serverGetCall(key);
-        } catch (Exception e) {
+        } 
+        catch (Exception e) 
+        {
             System.err.println("Client exception: " + e.toString());
             e.printStackTrace();
             return null;
