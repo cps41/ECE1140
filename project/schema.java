8342cb422235ab9e3a49a1b487e17bb1622daa20
csills <csills@zolllv.com>
2020-11-09 11:35:15 -0500

    schema

--- project/schema.java
+++ project/schema.java
@@ -1,10 +1,27 @@
-package project;
-
 public class schema {
-    public schema() {
+    public class TrainModel {
+        public final String velocity = "TrainModelVelocity";
+        public final String speed = "TrainModelSpeed";
+        public final String authority = "TrainModelAuthority";
+        public final String power = "TrainModelPower";
+        public final String length = "TrainModelLength";
+        public final String mass = "TrainModelMass";
+        public final String emergency_brake = "TrainModelEmergencyBrake";
+    }
+
+    public class TrainController {
         
     }
-    public class TrainModel {
 
+    public class TrackController {
+        
+    }
+
+    public class TrackModel {
+        
+    }
+
+    public class CTCOffice {
+        
     }
 }
