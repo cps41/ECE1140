bff3f9a3a3dc254ab0a184208850cd17bc0a6da7
Carly Sills <carlysills@Carlys-MacBook-Pro-8.local>
2020-11-11 18:15:23 -0500

    lots

--- TrainModelClient.java
+++ TrainModelClient.java
@@ -1,43 +1,60 @@
 /* Needed on Client only */
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.*;
 import javax.swing.*;
 import project.*;
 
 public class TrainModelClient {    
     private static final int PORTNUMBER = 0;    
-    private static final String cpuIP = "3.131.133.188";    
+    private static final String cpuIP = "18.216.251.172";    
     private static ServerInterface mini;    
     private static boolean connected = false;
 
     public static void main(String[] args) throws InterruptedException {
-        TrainModelGUI train = new TrainModelGUI();
-        send("power", TrainModel.POWER);
-        send("authority", TrainModel.AUTHORITY);
+        Object num_cars = receive(schema.TrackModel.cars);
+        if (num_cars == null) num_cars = 1;
+        TrainModelGUI train = new TrainModelGUI((int) num_cars);
+        send("power", 0f);
+        send("authority", 0f);
 
-        while(train.frame.isShowing()) {	 
-            System.out.println("Showing...");       
-	        //receive key inputs
-	        Object power = receive("power");
-            Object authority = receive("authority");
-            Object breaks = receive("breaks");
-            //send key outputs
-            if (authority == null) authority = 0.0f;
-            if (power == null) power = 0.0f;
-            if (breaks == null) breaks = false;
-            send("authority", authority);
-            send("velocity", (float) power*2);
+        while(train.frame.isShowing()) {     
+            //receive key inputs
+            Object power = new Object(), authority = new Object(), brake = new Object(), speed_limit = new Object(), speed = new Object(), 
+                   interior_lights = new Object(), exterior_lights = new Object(), left_doors = new Object(), right_doors = new Object(), 
+                   passenger_count = new Object(), crew_count = new Object(), block = new Object();
+
+            String[] keys = {schema.TrainController.power, schema.TrackModel.authority, schema.TrackModel.authority, 
+                             schema.TrackModel.speed_limit, schema.TrackModel.setpoint_speed, schema.TrainController.interior_lights, 
+                             schema.TrainController.exterior_lights, schema.TrainController.left_doors, schema.TrainController.right_doors, 
+                             schema.TrackModel.pass_count, schema.TrackModel.crew_count, schema.TrackModel.block, schema.TrackModel.cars};
+
+            HashMap<String, Object> inputs = new HashMap<>();
+            
+            // receive values for all keys
+            // check if null
+            // if so, go into failure mode
+            for(int i=0; i<keys.length; i++) {
+                inputs.put(keys[i], receive(keys[i]));
+                if (inputs.get(keys[i]) == null) {
+                    authority = 0.0f;
+                    power = 0.0f;
+                    brake = true;
+                }
+            }
+
             //set key inputs
-            train.refresh((float) authority, (float) power, (boolean) breaks);
+            train.refresh(inputs);
+
+
         
             //wait so screen is visible
             Thread.sleep(1000);
         }
-        send("authority", null);
-        send("velocity", null);
     }    
     static boolean send(String key, Object value) {        
-        if (!connected) {            connectClient();        
+        if (!connected) {            
+            connectClient();        
         }        
         try {            
             mini.serverSend(key, value);            
@@ -92,5 +109,8 @@ public class TrainModelClient {
             e.printStackTrace();            
             return null;        
         }    
-    }*/
+    }
+    ArrayList< Queue<boolean> >      */
+    //ArrayList
+    //int arr[] 
 }
